747f6bf96120029bb97ddd67a807fcfd9d514586
Murtada Nasser <murtada.nasser@ta3leem.dev>
2025-03-28 14:19:27 +0300

    feat : add 'AuthController' and define authentication routes

--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,22 @@
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TodoController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('todos', TodoController::class);
 
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+});
 Route::delete('users/{user}', function (User $user) {
     $user->delete();
     return response()->json(null, 204);
 });
+
+Route::apiResource('todos', TodoController::class)->middleware('auth:sanctum');
